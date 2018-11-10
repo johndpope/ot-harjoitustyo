3e8fc36164d204c48e0f1bd9868aba9ae50e3c16
Zentryn <zentryn@gmail.com>
2018-11-10 22:25:39 +0200

    Added tests for 2nd exercise

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,38 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void oikeaSaldo() {
+        assertEquals(kortti.saldo(), 10);
+    }
+    
+    @Test
+    public void saldonLisaysToimii() {
+        kortti.lataaRahaa(2);
+        assertEquals(kortti.saldo(), 12);
+        kortti.lataaRahaa(29);
+        assertEquals(kortti.saldo(), 41);
+    }
+    
+    @Test
+    public void rahanOttaminenToimii() {
+        kortti.otaRahaa(5);
+        assertEquals(kortti.saldo(), 5);
+        kortti.otaRahaa(4);
+        assertEquals(kortti.saldo(), 1);
+    }
+    
+    @Test
+    public void saldoEiMuutuJosEiTarpeeksiRahaa() {
+        boolean works1 = kortti.otaRahaa(11);
+        assertEquals(kortti.saldo(), 10);
+        boolean works2 = kortti.otaRahaa(10);
+        boolean works3 = kortti.otaRahaa(1);
+        assertEquals(kortti.saldo(), 0);
+        
+        assertFalse(works1);
+        assertTrue(works2);
+        assertFalse(works3);
+    }
 }
