632e8fab52ecd499af9692d753e65ca05c3b30df
Zentryn <zentryn@gmail.com>
2018-11-27 23:04:16 +0200

    Added logging and game ending

--- src/main/java/roguelike/ui/UIManager.java
+++ src/main/java/roguelike/ui/UIManager.java
@@ -1,5 +1,7 @@
 package roguelike.ui;
 
+import java.util.ArrayList;
+
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
@@ -12,17 +14,26 @@ import javafx.stage.Stage;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
+import javafx.util.Duration;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+
 import roguelike.domain.Level;
 import roguelike.domain.LevelManager;
+import roguelike.util.Logger;
 
 public class UIManager {
     private Stage window;
     private LevelManager levelManager;
     private Text[] levelTextData;
+    private Timeline delayFunc;
 
     // Size of characters on screen
     final int charHeight = 17;
     final int charWidth = 11;
+    final int nrLogLines = 5;
+    final int delayFuncMS = 50;
+    final int bottomTextSpace = charHeight * (nrLogLines + 1);
 
     // Extra padding in the window
     final int windowPadding = 10;
@@ -33,10 +44,29 @@ public class UIManager {
      */
     public UIManager(Stage stage) {
         this.window = stage;
+        UIManager self = this;
         this.levelManager = new LevelManager();
+        Logger.nrLines = this.nrLogLines;
+        Logger.setOnLog(() -> {
+            self.createLogTextData();
+            self.updateTextData();
+        });
 
         this.window.setTitle("Roguelike");
         this.window.setResizable(false);
+
+        // Delay function for when player moves
+        this.delayFunc = new Timeline(new KeyFrame(
+            Duration.millis(delayFuncMS),
+            ae -> {
+                Level l = self.levelManager.getCurrentLevel();
+
+                if (l != null) {
+                    l.moveEnemies();
+                    self.updateTextData();
+                }
+            }
+        ));
     }
     
     /**
@@ -61,7 +91,7 @@ public class UIManager {
         // Create scene and set its size to fit the level
         Scene scene = new Scene(
             grid, level.getWidth() * charWidth - windowPadding,
-            level.getHeight() * charHeight - windowPadding
+            level.getHeight() * charHeight - windowPadding + bottomTextSpace
         );
 
         scene.setFill(Color.BLACK);
@@ -79,16 +109,39 @@ public class UIManager {
      */
     private void createTextDataForLevel(Level level) {
         // Store the level in a Text object array so it can easily be modified
-        this.levelTextData = new Text[level.getHeight()];
+        this.levelTextData = new Text[level.getHeight() + this.nrLogLines + 1];
 
         // Setup text objects with fonts and colors
-        for (int i = 0; i < level.getHeight(); i++) {
+        for (int i = 0; i < this.levelTextData.length; i++) {
             Text t = new Text();
-            t.setText(new String(level.getRow(i)));
-            t.setFill(Color.WHITE);
+
+            if (i < level.getHeight()) {
+                t.setText(new String(level.getRow(i)));
+                t.setFill(Color.WHITE);
+            } else {
+                t.setText("");
+                t.setFill(Color.LIGHTCORAL);
+            }
+
             t.setY(i * charHeight);
             t.setFont(Font.font("consolas", FontWeight.BOLD, FontPosture.REGULAR, 20));
+            
             this.levelTextData[i] = t;
+        }
+
+        this.createLogTextData();
+    }
+
+    /**
+     * Creates log text data that can be then outputted to the screen
+     */
+    private void createLogTextData() {
+        int idx = this.levelTextData.length - 1;
+
+        // Update log messages
+        ArrayList<String> logMessages = Logger.messages;
+        for (int i = 0; i < logMessages.size(); i++) {
+            this.levelTextData[idx--].setText(logMessages.get(i));
         }
     }
 
@@ -128,21 +181,34 @@ public class UIManager {
      */
     private void onKeyPress(KeyCode keyCode) {
         if (keyCode == KeyCode.LEFT) {
-            this.levelManager.movePlayer(-1, 0);
+            movePlayer(0, -1);
         } else if (keyCode == KeyCode.RIGHT) {
-            this.levelManager.movePlayer(1, 0);
+            movePlayer(0, 1);
         } else if (keyCode == KeyCode.UP) {
-            this.levelManager.movePlayer(0, -1);
+            movePlayer(-1, 0);
         } else if (keyCode == KeyCode.DOWN) {
-            this.levelManager.movePlayer(0, 1);
+            movePlayer(1, 0);
         } else if (keyCode == KeyCode.DIGIT1) {
             this.levelManager.changeLevel(1);
-            this.showCurrentLevel();
-        } else if (keyCode == KeyCode.DIGIT2) {
-            this.levelManager.changeLevel(2);
             this.showCurrentLevel();
         }
 
         this.updateTextData();
+    }
+
+    /**
+     * Moves the player by a specified amount
+     * @param yDiff Amount the player should be moved in the y direction
+     * @param xDiff Amount the player should be moved in the x direction
+     */
+    private void movePlayer(int yDiff, int xDiff) {
+        if (this.levelManager.getCurrentLevel().player == null) {
+            this.window.close();
+            return;
+        }
+
+        if (this.levelManager.movePlayer(yDiff, xDiff)) {
+            this.delayFunc.play();
+        }
     }
 }
