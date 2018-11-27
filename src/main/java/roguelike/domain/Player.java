f5701847c20fb2acd541d1f3c250b2b879614c79
Zentryn <zentryn@gmail.com>
2018-11-27 23:03:19 +0200

    Added name variable

--- src/main/java/roguelike/domain/Player.java
+++ src/main/java/roguelike/domain/Player.java
@@ -1,9 +1,16 @@
 package roguelike.domain;
 
 public class Player extends Unit {
-    public Player(int y, int x, Level level) {
-        super(y, x, level);
+    public Player(int y, int x, String name, Level level) {
+        super(y, x, name, level);
         this.health = 10;
         this.damage = 4;
+    }
+
+    @Override
+    public boolean move(int yDiff, int xDiff) {
+        boolean success = super.move(yDiff, xDiff);
+
+        return success;
     }
 }
