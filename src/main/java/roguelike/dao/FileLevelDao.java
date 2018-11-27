3f939ea3be96eac4cb50324bc6bdb4bdf03ccc60
Zentryn <zentryn@gmail.com>
2018-11-27 16:04:02 +0200

    Reformatting to fix checkstyle errors

--- src/main/java/roguelike/dao/FileLevelDao.java
+++ src/main/java/roguelike/dao/FileLevelDao.java
@@ -16,34 +16,47 @@ public class FileLevelDao implements LevelDao {
     @Override
     public Level loadLevel(int level) {
         try {
-            // Load level file from savedata folder
-            Scanner reader = new Scanner(new File(this.appRootDir + "/savedata/levels/level" + level + ".txt"));
-
-            ArrayList<char[]> initialLevelData = new ArrayList<>();
-            while (reader.hasNextLine()) {
-                String line = reader.nextLine();
-
-                // Discard empty lines
-                if (line.trim().length() > 0)
-                    initialLevelData.add(line.toCharArray());
-            }
-
-            reader.close();
-
-            // Empty level file
-            if (initialLevelData.size() == 0) return null;
-
-            // Turn data into wanted char array format
-            char[][] levelData = new char[initialLevelData.size()][initialLevelData.get(0).length];
-            for (int i = 0; i < initialLevelData.size(); i++) {
-                levelData[i] = initialLevelData.get(i);
-            }
-
-            return new Level(levelData);
+            return this.readLevelFromFile(level);
         } catch (Exception e) {
             System.out.println("Error while reading level \"" + level + "\": " + e.getMessage());
 
             return null;
         }
+    }
+
+    /**
+     * Loads a level from a file into a Level object
+     * @param level Number of the level that should be loaded
+     * @return Created level object from the loaded data
+     * @throws Exception If level file was not found
+     */
+    private Level readLevelFromFile(int level) throws Exception {
+        // Load level file from savedata folder
+        Scanner reader = new Scanner(new File(this.appRootDir + "/savedata/levels/level" + level + ".txt"));
+
+        ArrayList<char[]> initialLevelData = new ArrayList<>();
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+
+            // Discard empty lines
+            if (line.trim().length() > 0) {
+                initialLevelData.add(line.toCharArray());
+            }
+        }
+
+        reader.close();
+
+        // Empty level file
+        if (initialLevelData.size() == 0) {
+            return null;
+        }
+
+        // Turn data into wanted char array format
+        char[][] levelData = new char[initialLevelData.size()][initialLevelData.get(0).length];
+        for (int i = 0; i < initialLevelData.size(); i++) {
+            levelData[i] = initialLevelData.get(i);
+        }
+
+        return new Level(levelData);
     }
 }
