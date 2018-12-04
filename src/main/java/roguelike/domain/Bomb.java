b831d2ec301038c68b89577c54bda9f041c8bd10
Zentryn <zentryn@gmail.com>
2018-12-04 22:05:05 +0200

    Added bomb item class

---
+++ src/main/java/roguelike/domain/Bomb.java
@@ -0,0 +1,16 @@
+package roguelike.domain;
+
+public class Bomb extends Item {
+    public int blastRadius;
+    public int blastDamage;
+
+    /**
+     * Create a new bomb
+     * @param holder The unit that has this bomb
+     */
+    public Bomb(Unit holder) {
+        super("Bomb", holder);
+        this.blastRadius = 2;
+        this.blastDamage = 6;
+    }
+}
