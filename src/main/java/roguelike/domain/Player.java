ba230dabc92c43dff2db35e889b9b658f8a7d28f
Zentryn <zentryn@gmail.com>
2018-12-04 22:03:27 +0200

    Improved player's stats for easier game

--- src/main/java/roguelike/domain/Player.java
+++ src/main/java/roguelike/domain/Player.java
@@ -3,8 +3,8 @@ package roguelike.domain;
 public class Player extends Unit {
     public Player(int y, int x, String name, Level level) {
         super(y, x, name, level);
-        this.health = 10;
-        this.damage = 4;
+        this.health = 12;
+        this.damage = 5;
     }
 
     @Override
